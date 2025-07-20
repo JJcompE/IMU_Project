3eb10d73e7efdde84d6a87d3fd7d02266dd45f4a
Jonathan Joyner <jon.joyner123@gmail.com>
2025-07-19 22:28:06 -0400

    updates

---
+++ mcu/Core/Inc/imu1.h
@@ -0,0 +1,29 @@
+#include <cstdint>
+
+const uint8_t IMU1_I2C_ADDRESS = 0x6B;
+const uint8_t IMU1_I2C_ADDRESS_WR = 0xD7;
+const uint8_t IMU1_WHO_AM_I_REGISTER = 0x0F;
+const uint8_t IMU1_DEVICE_ID = 0x70;
+const uint8_t IMU1_Acc_Cntr1 = 0x10;
+const uint8_t IMU1_Gyro_Cntr2 = 0x11;
+const uint8_t IMU1_Status = 0x1E;
+const uint8_t IMU1_CTRL3_REG = 0x12;
+const uint8_t IMU1_FUNC_CFG_ACCESS = 0x01;
+const uint8_t IMU1_CTRL6 = 0x15;
+
+const float GRAVITY = 9.81;
+const float ACCEL_SENS = 0.061;
+const float GYRO_SENS = 4.375;
+
+
+#include <stdint.h>
+
+int16_t convert_to_int16(uint8_t high, uint8_t low) {
+    return (int16_t)((high << 8) | low);
+}
+
+void convert_accel_data(int16_t *raw_accel_ini, const uint8_t *a_buff) {
+    for (int i = 0; i < 3; ++i) {
+        raw_accel_ini[i] = convert_to_int16(a_buff[2 * i + 1], a_buff[2 * i]);
+    }
+}
--- mcu/Core/Src/main.c
+++ mcu/Core/Src/main.c
@@ -111,7 +111,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-
+    uint8_t init_ret_1 = init_imu_1();
+    get_imu1_data();
   }
   /* USER CODE END 3 */
 }
@@ -351,7 +352,103 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+uint8_t init_imu_1() {
+	uint8_t read_dev_id = 0;
+  uint8_t reboot_mem_bm_val = 0x80;
+  uint8_t hal_status = HAL_I2C_Mem_Read(&hi2c1, (IMU1_I2C_ADDRESS << 1), IMU1_WHO_AM_I_REGISTER, 1, &read_dev_id, 1, HAL_MAX_DELAY);
 
+  if (hal_status != HAL_OK) {
+    return hal_status;
+  }
+
+  if (read_dev_id == IMU1_DEVICE_ID) {
+    HAL_I2C_Mem_Write(&hi2c1, IMU1_I2C_ADDRESS_WR, IMU1_CTRL3_REG, 1, &reboot_mem_bm_val, 1, HAL_MAX_DELAY); // reboot data in IMU
+    return 0;
+  }
+
+  return -1;
+}
+
+void get_imu1_data(void) {
+	int16_t raw_accel_ini[3] = 0;
+	int16_t raw_ang_ini[3] = 0;
+	uint8_t OUTX_L_G = 0x22, OUTX_L_A = 0x28; //Register addresses - lower 8 bits
+	uint8_t OUTX_H_G = 0x23, OUTX_H_A = 0x29; //Register addresses - higher 8 bits
+	uint8_t OUTY_L_G = 0x24, OUTY_L_A = 0x2A;
+	uint8_t OUTY_H_G = 0x25, OUTY_H_A = 0x2B;
+	uint8_t OUTZ_L_G = 0x26, OUTZ_L_A = 0x2C;
+	uint8_t OUTZ_H_G = 0x27, OUTZ_H_A = 0x2D;
+	uint8_t g_buff[6] = 0, a_buff[6] = 0, status_buff[8] = 0; //Temp buffers
+	uint8_t acc_stat, gyro_stat;
+
+	//Read data status buffer
+	HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), IMU_Status, 1, status_buff, 1, HAL_MAX_DELAY);
+
+	//Set bits of data flags
+	acc_stat = status_buff[0] & 0x01;
+	gyro_stat = (status_buff[0] >> 1) & 0x01;
+
+	//Acceleration data
+	if (acc_stat == 1) { //If new data ready at accelerometer
+		//Read accelerometer X data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTX_L_A, 1, &a_buff[0], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTX_H_A, 1, &a_buff[1], 1, HAL_MAX_DELAY);
+		//Read accelerometer Y data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTY_L_A, 1, &a_buff[2], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTY_H_A, 1, &a_buff[3], 1, HAL_MAX_DELAY);
+		//Read accelerometer Z data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTZ_L_A, 1, &a_buff[4], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTZ_H_A, 1, &a_buff[5], 1, HAL_MAX_DELAY);
+
+		for (int i = 0; i < 3; i++) {
+      raw_accel_ini[i] = (int16_t) ((a_buff[2 * i + 1] << 8) | a_buff[2 * i]);
+    }
+	}
+
+	//Gyroscope data
+	if (gyro_stat == 1) { //If new data ready at gyro
+		//Read gyroscope X data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTX_L_G, 1, &g_buff[0], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTX_H_G, 1, &g_buff[1], 1, HAL_MAX_DELAY);
+		//Read gyroscope Y data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTY_L_G, 1, &g_buff[2], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTY_H_G, 1, &g_buff[3], 1, HAL_MAX_DELAY);
+		//Read gyroscope Z data
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTZ_L_G, 1, &g_buff[4], 1, HAL_MAX_DELAY);
+		HAL_I2C_Mem_Read(&hi2c1, (IMU_I2C_ADDRESS << 1), OUTZ_H_G, 1, &g_buff[5], 1, HAL_MAX_DELAY);
+
+		for (int i = 0; i < 3; i++) {
+      raw_ang_ini[i] = (int16_t)((g_buff[2 * i + 1] << 8) | g_buff[2 * i]);
+    }
+	}
+
+  for (int i = 0; i < 3; i++) {
+    raw_accel[i] = (raw_accel_ini[i] * ACCEL_SENS) / 1000;
+    raw_ang[i] = (raw_ang[i] * GYRO_SENS) / 1000;
+  }
+}
+
+void tx_all_data(float *accel, float *angular) {
+	static uint8_t tx_buffer[100];
+
+	sprintf((char *)tx_buffer, "Accel rate X axis [+/-2g]: %f\r\n", accel[0]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+	sprintf((char *)tx_buffer, "Accel rate Y axis [+/-2g]: %f\r\n", accel[1]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+	sprintf((char *)tx_buffer, "Accel rate Z axis [+/-2g]: %f\r\n", accel[2]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+
+	sprintf((char *)tx_buffer, "Angular rate X axis [deg/sec]: %f\r\n", angular[0]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+	sprintf((char *)tx_buffer, "Angular rate Y axis [deg/sec]: %f\r\n", angular[1]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+	sprintf((char *)tx_buffer, "Angular rate Z axis [deg/sec]: %f\r\n", angular[2]);
+	HAL_UART_Transmit(&huart1, (uint8_t *)tx_buffer, strlen((char const *)tx_buffer), 1000);
+
+	// DEBUG DELAY TO BE ABLE TO READ SERIAL TERMINAL
+	HAL_Delay(500);
+
+}
 /* USER CODE END 4 */
 
 /**
